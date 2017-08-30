0766f84a439e05752fe08f37e0c1854ca36bcd7c
danizaghian <dani.zaghian@gmail.com>
2017-08-29 21:32:17 -0700

    query parameter solutions

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -4,4 +4,28 @@ class ParamsExamplesController < ApplicationController
 		@number = params[:my_number]
 		render "query_params.html.erb"
 	end
+
+	def name_method
+		@my_name = "Enter a name"
+		if params[:my_name]
+			@my_name = params[:my_name].upcase
+			if @my_name.starts_with?("A")
+				@message = "Hey, your name starts with an A!"
+			end
+		end
+		render "name.html.erb"
+	end
+
+	def guess_game_method
+		winning_number = 70
+		@guess = params[:my_guess].to_i
+		if @guess > winning_number
+			@message = "Guess lower!"
+		elsif @guess < winning_number
+			@message = "Guess higher!"
+		else
+			@message = "You win!"
+		end
+		render "guess_game.html.erb"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/query_params" => "params_examples#query_params_method"
+  get "/name" => "params_examples#name_method"
+  get "/guess_game" => "params_examples#guess_game_method"
 end
